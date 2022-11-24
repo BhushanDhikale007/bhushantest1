a7229558c3a9825b128bd3161cf6c429655899cd
BhushanDhikale007 <101262292+BhushanDhikale007@users.noreply.github.com>
2022-11-25 00:27:19 +0530

    Delete logicanc.tf

--- logicanc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "compliant-bucket-2" {
-  bucket = "sid-compliant-test-bucketk2"
-  acl    = "public"
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
